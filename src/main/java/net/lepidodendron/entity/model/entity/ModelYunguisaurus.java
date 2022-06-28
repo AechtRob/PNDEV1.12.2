ed70fe76014d29809862fb25a376bd6321b3061e
AechtRob <aecht_rob@outlook.com>
2022-06-28 22:26:38 +0100

    Phials and additions

--- src/main/java/net/lepidodendron/entity/model/entity/ModelYunguisaurus.java
+++ src/main/java/net/lepidodendron/entity/model/entity/ModelYunguisaurus.java
@@ -1,6 +1,7 @@
 package net.lepidodendron.entity.model.entity;
 
 import net.ilexiconn.llibrary.client.model.tools.AdvancedModelBase;
+import net.lepidodendron.entity.EntityPrehistoricFloraYunguisaurus;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
@@ -29,6 +30,40 @@ public class ModelYunguisaurus extends AdvancedModelBase {
 
     @Override
     public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity e) {
+
+        float Finvertcialangle = 1;
+
+        EntityPrehistoricFloraYunguisaurus ee = (EntityPrehistoricFloraYunguisaurus) e;
+        int CycleA1 = 8;
+        int CycleA2 = 2;
+        int CycleA3 = 8;
+        int CycleA4 = 2;
+
+        int tickA = ee.getCycleTick();
+        double revisedtickA = 0;
+
+        if (tickA <= CycleA1) {
+            revisedtickA = tickA;
+        }
+        else {
+            if (tickA > (CycleA1 + CycleA2) && tickA <= (CycleA1 + CycleA2 + CycleA3)) {
+                revisedtickA = (CycleA1 + CycleA2 + CycleA3) - tickA + 1;
+            }
+            else {
+                if (tickA > (CycleA1 + CycleA2)) {
+                    revisedtickA = 0;
+                }
+                else {
+                    revisedtickA = CycleA1;
+                }
+            }
+        }
+        double percenttickA = revisedtickA / CycleA1;
+        double radiansA = percenttickA * (Math.PI * 0.25 * 2);
+        float angleA = (float)Math.sin(radiansA);
+
+        System.err.println("angleA " + angleA);
+        
         //stuff here
     }
 }
