25add8c914359d4b05ede1735fa32708bbb2a5ac
AechtRob <aecht_rob@outlook.com>
2022-07-23 13:43:57 +0100

    Fixes and tweaks

--- src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraFalcatus.java
+++ src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraFalcatus.java
@@ -34,12 +34,12 @@ public class EntityPrehistoricFloraFalcatus extends EntityPrehistoricFloraAgeabl
 	public BlockPos currentTarget;
 	@SideOnly(Side.CLIENT)
 
-	private static final DataParameter<Boolean> ISFEMALE = EntityDataManager.createKey(EntityPrehistoricFloraShringasaurus.class, DataSerializers.BOOLEAN);
+	private static final DataParameter<Boolean> ISFEMALE = EntityDataManager.createKey(EntityPrehistoricFloraFalcatus.class, DataSerializers.BOOLEAN);
 	public ChainBuffer chainBuffer;
 
 	public EntityPrehistoricFloraFalcatus(World world) {
 		super(world);
-		//setSize(0.4F, 0.4F);
+		setSize(0.1F, 0.2F);
 		experienceValue = 0;
 		this.isImmuneToFire = false;
 		setNoAI(!true);
--- src/main/java/net/lepidodendron/util/Summoner.java
+++ src/main/java/net/lepidodendron/util/Summoner.java
@@ -103,7 +103,7 @@ public class Summoner extends CommandBase
                 }
                 else
                 {
-                    entity.setLocationAndAngles(d0 + 0.5, d1, d2 + 0.5, entity.rotationYaw, entity.rotationPitch);
+                    entity.setLocationAndAngles(d0 + 0.5 + (Math.random() - 0.5), d1, d2 + 0.5 + (Math.random() - 0.5), entity.rotationYaw, entity.rotationPitch);
 
                     if (!flag && entity instanceof EntityLiving)
                     {
