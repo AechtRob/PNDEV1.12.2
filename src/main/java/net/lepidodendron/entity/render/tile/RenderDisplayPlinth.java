6f1f35047551237b9df40daf6ee889bf5225085f
Tethyshadros <blackfrog@hotmail.it>
2022-08-05 17:31:07 +0200

    Added Taxidermies for Arizonasaurus, Effigia, Eorhynchochelys, Smilosuchus, Uranocentrodon and Vetulicola

--- src/main/java/net/lepidodendron/entity/render/tile/RenderDisplayPlinth.java
+++ src/main/java/net/lepidodendron/entity/render/tile/RenderDisplayPlinth.java
@@ -76,8 +76,12 @@ public class RenderDisplayPlinth extends TileEntitySpecialRenderer<BlockDisplayP
     private final ModelDvinia modelDvinia;
     private static final ResourceLocation TEXTURE_DVINOSAURUS = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/dvinosaurus.png");
     private final ModelDvinosaurus modelDvinosaurus;
+    private static final ResourceLocation TEXTURE_EFFIGIA = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/effigia.png");
+    private final ModelEffigia modelEffigia;
     private static final ResourceLocation TEXTURE_ELGINIA = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/elginia.png");
     private final ModelElginia modelElginia;
+    private static final ResourceLocation TEXTURE_EORHYNCHOCHELYS = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/eorhynchochelys.png");
+    private final ModelEorhynchochelys modelEorhynchochelys;
     private static final ResourceLocation TEXTURE_EOSIMOPS = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/eosimops.png");
     private static final ResourceLocation TEXTURE_EUCHAMBERSIA = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/euchambersia.png");
     private final ModelEuchambersia modelEuchambersia;
@@ -186,7 +190,9 @@ public class RenderDisplayPlinth extends TileEntitySpecialRenderer<BlockDisplayP
         this.modelDiploceraspis = new ModelDiploceraspis();
         this.modelDvinia = new ModelDvinia();
         this.modelDvinosaurus = new ModelDvinosaurus();
+        this.modelEffigia = new ModelEffigia();
         this.modelElginia = new ModelElginia();
+        this.modelEorhynchochelys = new ModelEorhynchochelys();
         this.modelEuchambersia = new ModelEuchambersia();
         this.modelEudimorphodon = new ModelEudimorphodon();
         this.modelEuparkeria = new ModelEuparkeria();
@@ -524,6 +530,17 @@ public class RenderDisplayPlinth extends TileEntitySpecialRenderer<BlockDisplayP
                         GlStateManager.scale(0.3F,0.3F,0.3F);
                         modelDvinosaurus.renderStatic(Minecraft.getMinecraft().player.ticksExisted);
                     }
+                    else if (itemstack.getItem() == ItemEffigiaRaw.block) {
+                        double offset = 0.54;
+                        //double voffset = -0.15;
+                        double voffset = 0;
+                        GlStateManager.translate(x + 0.5, y + 0.5 + offset, z + 0.5 + voffset);
+                        GlStateManager.rotate(180, 0F, 0F, 1F);
+                        GlStateManager.rotate(currentRotation, 0F, 1F, 0F);
+                        this.bindTexture(TEXTURE_EFFIGIA);
+                        GlStateManager.scale(0.22F,0.22F,0.22F);
+                        modelEffigia.renderStatic(Minecraft.getMinecraft().player.ticksExisted);
+                    }
                     else if (itemstack.getItem() == ItemElginiaRaw.block) {
                         double offset = 0.55;
                         //double voffset = -0.15;
@@ -534,6 +551,17 @@ public class RenderDisplayPlinth extends TileEntitySpecialRenderer<BlockDisplayP
                         this.bindTexture(TEXTURE_ELGINIA);
                         GlStateManager.scale(0.23F,0.23F,0.23F);
                         modelElginia.renderStatic(Minecraft.getMinecraft().player.ticksExisted);
+                    }
+                    else if (itemstack.getItem() == ItemEorhynchochelysRaw.block) {
+                        double offset = 0.62;
+                        //double voffset = -0.15;
+                        double voffset = 0;
+                        GlStateManager.translate(x + 0.5, y + 0.5 + offset, z + 0.5 + voffset);
+                        GlStateManager.rotate(180, 0F, 0F, 1F);
+                        GlStateManager.rotate(currentRotation, 0F, 1F, 0F);
+                        this.bindTexture(TEXTURE_EORHYNCHOCHELYS);
+                        GlStateManager.scale(0.26F,0.26F,0.26F);
+                        modelEorhynchochelys.renderStatic(Minecraft.getMinecraft().player.ticksExisted);
                     }
                     else if (itemstack.getItem() == ItemEuchambersiaRaw.block) {
                         double offset = 0.47;
