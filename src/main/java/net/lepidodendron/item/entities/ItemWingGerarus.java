7287f95df2c336e871696409781206b113d3ca79
jyang077 <112518069+jyang077@users.noreply.github.com>
2022-10-21 13:00:50 -0400

    Oredict

--- src/main/java/net/lepidodendron/item/entities/ItemWingGerarus.java
+++ src/main/java/net/lepidodendron/item/entities/ItemWingGerarus.java
@@ -12,9 +12,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 
 import java.util.List;
 
@@ -30,7 +32,11 @@ public class ItemWingGerarus extends ElementsLepidodendronMod.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
+	@Override
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
+		OreDictionary.registerOre("dnaPNGerarus", ItemWingGerarus.block);
+	}
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
