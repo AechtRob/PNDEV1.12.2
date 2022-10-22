1cb41d97bc0e02bafb1ee52bfe3e6087b0981da2
Tethyshadros <blackfrog@hotmail.it>
2022-10-22 19:29:46 +0200

    Added oreDict to all D animals

--- src/main/java/net/lepidodendron/item/entities/ItemDianiaRaw.java
+++ src/main/java/net/lepidodendron/item/entities/ItemDianiaRaw.java
@@ -9,9 +9,11 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 
 @ElementsLepidodendronMod.ModElement.Tag
 public class ItemDianiaRaw extends ElementsLepidodendronMod.ModElement {
@@ -30,6 +32,13 @@ public class ItemDianiaRaw extends ElementsLepidodendronMod.ModElement {
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
 		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("lepidodendron:entities/diania_raw", "inventory"));
+	}
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
+		OreDictionary.registerOre("dnaPNDiania", ItemDianiaRaw.block);
+		OreDictionary.registerOre("listAllfishraw", ItemDianiaRaw.block);
+		OreDictionary.registerOre("listAllmeatraw", ItemDianiaRaw.block);
+		OreDictionary.registerOre("foodMeat", ItemDianiaRaw.block);
 	}
 	public static class ItemFoodCustom extends ItemFood {
 		public ItemFoodCustom() {
--- src/main/java/net/lepidodendron/item/entities/ItemDrotopsRaw.java
+++ src/main/java/net/lepidodendron/item/entities/ItemDrotopsRaw.java
@@ -9,9 +9,11 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 
 @ElementsLepidodendronMod.ModElement.Tag
 public class ItemDrotopsRaw extends ElementsLepidodendronMod.ModElement {
@@ -30,6 +32,13 @@ public class ItemDrotopsRaw extends ElementsLepidodendronMod.ModElement {
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
 		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("lepidodendron:entities/drotops_raw", "inventory"));
+	}
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
+		OreDictionary.registerOre("dnaPNDrotops", ItemDrotopsRaw.block);
+		OreDictionary.registerOre("listAllfishraw", ItemDrotopsRaw.block);
+		OreDictionary.registerOre("listAllmeatraw", ItemDrotopsRaw.block);
+		OreDictionary.registerOre("foodMeat", ItemDrotopsRaw.block);
 	}
 
 	public static class ItemFoodCustom extends ItemFood {
