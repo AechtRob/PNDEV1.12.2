1bb8b574791deacae811968e2a2006c6311c72bf
AechtRob <aecht_rob@outlook.com>
2022-12-04 09:07:45 +0000

    Precambrian support

--- src/main/java/net/lepidodendron/LepidodendronOreHandler.java
+++ src/main/java/net/lepidodendron/LepidodendronOreHandler.java
@@ -28,15 +28,18 @@ public class LepidodendronOreHandler {
 			)
             	event.setResult(Event.Result.DENY);
 		}
-		if (event.getType().equals(EventType.IRON)){
-			if (event.getWorld().getBiome( event.getPos().add(8,0,8)).getRegistryName().toString().equalsIgnoreCase("lepidodendron:paleoproterozoic_beach")
-				|| event.getWorld().getBiome( event.getPos().add(8,0,8)).getRegistryName().toString().equalsIgnoreCase("lepidodendron:paleoproterozoic_regolith")
-				|| event.getWorld().getBiome( event.getPos().add(8,0,8)).getRegistryName().toString().equalsIgnoreCase("lepidodendron:paleoproterozoic_shallows")) {
-				genStandardOre1(event.getPos(), event.getWorld(), event.getRand(), 50,
-						new WorldGenMinable(Blocks.IRON_ORE.getDefaultState(), 9),
-						100, 140);
+		if (event.getType().equals(EventType.IRON)) {
+			if (LepidodendronConfig.dimPrecambrian == (int) event.getWorld().provider.getDimension()
+			) {
+				if (event.getWorld().getBiome( event.getPos().add(8,0,8)).getRegistryName().toString().equalsIgnoreCase("lepidodendron:paleoproterozoic_beach")
+					|| event.getWorld().getBiome( event.getPos().add(8,0,8)).getRegistryName().toString().equalsIgnoreCase("lepidodendron:paleoproterozoic_regolith")
+					|| event.getWorld().getBiome( event.getPos().add(8,0,8)).getRegistryName().toString().equalsIgnoreCase("lepidodendron:paleoproterozoic_shallows")) {
+					genStandardOre1(event.getPos(), event.getWorld(), event.getRand(), 50,
+							new WorldGenMinable(Blocks.IRON_ORE.getDefaultState(), 9),
+							100, 140);
+				}
+				event.setResult(Event.Result.DENY);
 			}
-			event.setResult(Event.Result.DENY);
 		}
 		if (event.getType().equals(EventType.DIRT )){ //dirt is blocked to avoid grass etc accidentally appearring
 			if (LepidodendronConfig.dimCretaceous == (int) event.getWorld().provider.getDimension()
@@ -62,18 +65,22 @@ public class LepidodendronOreHandler {
 			}
 		}
 		if (event.getType().equals(EventType.GRANITE )){
-
-			genStandardOre1(event.getPos(), event.getWorld(), event.getRand(), 10,
-					new WorldGenMinable(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE), 33),
-					105, 170);
-			event.setResult(Event.Result.DENY);
+			if (LepidodendronConfig.dimPrecambrian == (int) event.getWorld().provider.getDimension()
+			) {
+				genStandardOre1(event.getPos(), event.getWorld(), event.getRand(), 10,
+						new WorldGenMinable(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE), 33),
+						105, 170);
+				event.setResult(Event.Result.DENY);
+			}
 		}
 		if (event.getType().equals(EventType.DIORITE )){
-
-			genStandardOre1(event.getPos(), event.getWorld(), event.getRand(), 10,
-				new WorldGenMinable(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE), 33),
-					105, 170);
-			event.setResult(Event.Result.DENY);
+			if (LepidodendronConfig.dimPrecambrian == (int) event.getWorld().provider.getDimension()
+			) {
+				genStandardOre1(event.getPos(), event.getWorld(), event.getRand(), 10,
+						new WorldGenMinable(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE), 33),
+						105, 170);
+				event.setResult(Event.Result.DENY);
+			}
 		}
 		if (event.getType().equals(EventType.SILVERFISH )){
 			if (LepidodendronConfig.dimDevonian == (int) event.getWorld().provider.getDimension()
