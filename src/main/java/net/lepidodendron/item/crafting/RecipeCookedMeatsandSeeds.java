c20587133884d1d44671db6bb168603726d875e1
AechtRob <59179133+AechtRob@users.noreply.github.com>
2022-12-23 09:46:07 +0000

    Update RecipeCookedMeatsandSeeds.java

--- src/main/java/net/lepidodendron/item/crafting/RecipeCookedMeatsandSeeds.java
+++ src/main/java/net/lepidodendron/item/crafting/RecipeCookedMeatsandSeeds.java
@@ -465,6 +465,7 @@ public class RecipeCookedMeatsandSeeds extends ElementsLepidodendronMod.ModEleme
 		GameRegistry.addSmelting(new ItemStack(ItemWhatcheeriaRaw.block, (int) (1)), new ItemStack(ItemWhatcheeriaCooked.block, (int) (1)), 0.35F);
 		GameRegistry.addSmelting(new ItemStack(ItemWingertshellicusRaw.block, (int) (1)), new ItemStack(ItemWingertshellicusCooked.block, (int) (1)), 0.35F);
 		GameRegistry.addSmelting(new ItemStack(ItemXinpusaurusRaw.block, (int) (1)), new ItemStack(ItemXinpusaurusCooked.block, (int) (1)), 0.35F);
+		GameRegistry.addSmelting(new ItemStack(ItemMegalosaurusRaw.block, (int) (1)), new ItemStack(ItemMegalosaurusCooked.block, (int) (1)), 0.35F);
 		GameRegistry.addSmelting(new ItemStack(ItemMacromesodonRaw.block, (int) (1)), new ItemStack(ItemMacromesodonCooked.block, (int) (1)), 0.35F);
 		GameRegistry.addSmelting(new ItemStack(ItemArduafronsRaw.block, (int) (1)), new ItemStack(ItemArduafronsCooked.block, (int) (1)), 0.35F);
 		GameRegistry.addSmelting(new ItemStack(ItemAspidorhynchusRaw.block, (int) (1)), new ItemStack(ItemAspidorhynchusCooked.block, (int) (1)), 0.35F);
