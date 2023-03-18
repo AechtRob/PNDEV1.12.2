c33a1f27cfa004197a5c8b3114c26b3c8df140c7
Tethyshadros <blackfrog@hotmail.it>
2023-03-18 15:39:21 +0100

    Crassigyrinus scaler

--- src/main/java/net/lepidodendron/entity/render/entity/RenderCrassigyrinus.java
+++ src/main/java/net/lepidodendron/entity/render/entity/RenderCrassigyrinus.java
@@ -10,6 +10,7 @@ import net.minecraft.util.ResourceLocation;
 
 public class RenderCrassigyrinus extends RenderLiving<EntityPrehistoricFloraCrassigyrinus> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/crassigyrinus.png");
+    public static float getScaler() {return 0.465F;}
 
     public RenderCrassigyrinus(RenderManager mgr) {
         super(mgr, new ModelCrassigyrinus(), 0.5f);
@@ -27,7 +28,7 @@ public class RenderCrassigyrinus extends RenderLiving<EntityPrehistoricFloraCras
 
     @Override
     protected void preRenderCallback(EntityPrehistoricFloraCrassigyrinus entity, float f) {
-        float scale = entity.getAgeScale();
+        float scale = entity.getAgeScale()*getScaler();
         GlStateManager.scale(scale, scale, scale);
         this.shadowSize = entity.width * scale * 0.21F;
     }
