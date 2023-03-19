3fe8d5cc149ef474ae737b126a082d31d706fe73
AechtRob <aecht_rob@outlook.com>
2023-03-19 22:03:33 +0000

    v.56.0 Boaty McBoatface

--- src/main/java/net/lepidodendron/block/BlockAraucaritesSapling.java
+++ src/main/java/net/lepidodendron/block/BlockAraucaritesSapling.java
@@ -208,7 +208,7 @@ public class BlockAraucaritesSapling extends ElementsLepidodendronMod.ModElement
 	    public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced) {
 	        if (LepidodendronConfig.showTooltips) {
 				tooltip.add("Type: Coniferous tree");
-				tooltip.add("Periods: Jurassic");
+				tooltip.add("Periods: Jurassic - Cretaceous - Paleogene");
 				tooltip.add("Propagation: fruit/cone");}
 	        super.addInformation(stack, player, tooltip, advanced);
 	    }
