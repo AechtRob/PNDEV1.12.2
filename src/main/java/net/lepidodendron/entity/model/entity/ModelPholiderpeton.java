a4b8de13daf4e7439beb9196cb114d1b0cc347dd
AechtRob <aecht_rob@outlook.com>
2023-03-22 22:03:54 +0000

    v.56.0 Fixes

--- src/main/java/net/lepidodendron/entity/model/entity/ModelPholiderpeton.java
+++ src/main/java/net/lepidodendron/entity/model/entity/ModelPholiderpeton.java
@@ -337,7 +337,7 @@ public class ModelPholiderpeton extends AdvancedModelBaseExtended {
     public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity e) {
         super.setRotationAngles(f, f1, f2, f3, f4, f5, e);
         this.resetToDefaultPose();
-        this.Hips.offsetY = 0.485F;
+        //this.Hips.offsetY = 0.485F;
         this.Hips.offsetZ = 0.45F;
 
         EntityPrehistoricFloraPholiderpeton Pholiderpeton = (EntityPrehistoricFloraPholiderpeton) e;
