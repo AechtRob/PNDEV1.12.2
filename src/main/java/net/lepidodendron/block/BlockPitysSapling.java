e46b5c1cb749d46c4d319fa5efafb1ca364d037e
AechtRob <aecht_rob@outlook.com>
2023-04-06 17:10:00 +0100

    v.56.0 Fixes

--- src/main/java/net/lepidodendron/block/BlockPitysSapling.java
+++ src/main/java/net/lepidodendron/block/BlockPitysSapling.java
@@ -199,7 +199,7 @@ public class BlockPitysSapling extends ElementsLepidodendronMod.ModElement {
 	    public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced) {
 	        if (LepidodendronConfig.showTooltips) {
 				tooltip.add("Type: Seed-Plant tree");
-				tooltip.add("Periods: Carboniferous)");
+				tooltip.add("Periods: Carboniferous");
 				tooltip.add("Propagation: seeds");}
 	        super.addInformation(stack, player, tooltip, advanced);
 	    }
