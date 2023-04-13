b64ac291ccf8e74b29e80e8897b065aedb3fda80
Tethyshadros <blackfrog@hotmail.it>
2023-04-14 00:49:35 +0200

    Sidneyia and Vetulicola scaler

--- src/main/java/net/lepidodendron/entity/render/entity/RenderVetulicola.java
+++ src/main/java/net/lepidodendron/entity/render/entity/RenderVetulicola.java
@@ -1,14 +1,17 @@
 package net.lepidodendron.entity.render.entity;
 
 import net.lepidodendron.LepidodendronMod;
+import net.lepidodendron.entity.EntityPrehistoricFloraBobasatrania;
 import net.lepidodendron.entity.EntityPrehistoricFloraVetulicola;
 import net.lepidodendron.entity.model.entity.ModelVetulicola;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderVetulicola extends RenderLiving<EntityPrehistoricFloraVetulicola> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/vetulicola.png");
+    public static float getScaler() {return 0.25F;}
 
     public RenderVetulicola(RenderManager mgr) {
         super(mgr, new ModelVetulicola(), 0.0f);
@@ -23,5 +26,12 @@ public class RenderVetulicola extends RenderLiving<EntityPrehistoricFloraVetulic
     protected void applyRotations(EntityPrehistoricFloraVetulicola entityLiving, float ageInTicks, float rotationYaw, float partialTicks) {
         super.applyRotations(entityLiving, ageInTicks, rotationYaw, partialTicks);
     }
-
+    protected void preRenderCallback(EntityPrehistoricFloraVetulicola entity, float f) {
+        float scale = this.getScaler();
+        if (scale < 0.1f) {
+            scale = 0.1f;
+        }
+        GlStateManager.scale(scale, scale, scale);
+        this.shadowSize = 0;
+    }
 }
