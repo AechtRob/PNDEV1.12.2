0303baadad0685d9814ff051eaa888ce807d6112
jyang077 <112518069+jyang077@users.noreply.github.com>
2023-04-14 13:50:57 -0400

    Items

--- src/main/java/net/lepidodendron/item/entities/ItemParamblypterusRaw.java
+++ src/main/java/net/lepidodendron/item/entities/ItemParamblypterusRaw.java
@@ -1,51 +1,51 @@
-//
-//package net.lepidodendron.item.entities;
-//
-//import net.lepidodendron.ElementsLepidodendronMod;
-//import net.lepidodendron.LepidodendronSorter;
-//import net.lepidodendron.creativetab.TabLepidodendronMobile;
-//import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-//import net.minecraft.item.Item;
-//import net.minecraftforge.client.event.ModelRegistryEvent;
-//import net.minecraftforge.client.model.ModelLoader;
-//import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-//import net.minecraftforge.fml.common.registry.GameRegistry;
-//import net.minecraftforge.fml.relauncher.Side;
-//import net.minecraftforge.fml.relauncher.SideOnly;
-//import net.minecraftforge.oredict.OreDictionary;
-//
-//@ElementsLepidodendronMod.ModElement.Tag
-//public class ItemParamblypterusRaw extends ElementsLepidodendronMod.ModElement {
-//	@GameRegistry.ObjectHolder("lepidodendron:paramblypterus_raw")
-//	public static final Item block = null;
-//	public ItemParamblypterusRaw(ElementsLepidodendronMod instance) {
-//		super(instance, LepidodendronSorter.paramblypterus_raw);
-//	}
-//
-//	@Override
-//	public void initElements() {
-//		elements.items.add(() -> new ItemFoodCustom());
-//	}
-//
-//	@SideOnly(Side.CLIENT)
-//	@Override
-//	public void registerModels(ModelRegistryEvent event) {
-//		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("lepidodendron:entities/paramblypterus_raw", "inventory"));
-//	}
-//	public void init(FMLInitializationEvent event) {
-//		super.init(event);
-//		OreDictionary.registerOre("mobdnaPNlepidodendron:prehistoric_flora_paramblypterus", ItemParamblypterusRaw.block);
-//		OreDictionary.registerOre("listAllfishraw", ItemParamblypterusRaw.block);
-//		OreDictionary.registerOre("listAllmeatraw", ItemParamblypterusRaw.block);
-//		OreDictionary.registerOre("foodMeat", ItemParamblypterusRaw.block);
-//	}
-//	public static class ItemFoodCustom extends ItemPNTaxidermyItem {
-//		public ItemFoodCustom() {
-//			super(2, 0.1f, false);
-//			setTranslationKey("pf_paramblypterus_raw");
-//			setRegistryName("paramblypterus_raw");
-//			setCreativeTab(TabLepidodendronMobile.tab);
-//			setMaxStackSize(64);
-//		}
-//	}
-//}
+
+package net.lepidodendron.item.entities;
+
+import net.lepidodendron.ElementsLepidodendronMod;
+import net.lepidodendron.LepidodendronSorter;
+import net.lepidodendron.creativetab.TabLepidodendronMobile;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
+
+@ElementsLepidodendronMod.ModElement.Tag
+public class ItemParamblypterusRaw extends ElementsLepidodendronMod.ModElement {
+	@GameRegistry.ObjectHolder("lepidodendron:paramblypterus_raw")
+	public static final Item block = null;
+	public ItemParamblypterusRaw(ElementsLepidodendronMod instance) {
+		super(instance, LepidodendronSorter.paramblypterus_raw);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemFoodCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("lepidodendron:entities/paramblypterus_raw", "inventory"));
+	}
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
+		OreDictionary.registerOre("mobdnaPNlepidodendron:prehistoric_flora_paramblypterus", ItemParamblypterusRaw.block);
+		OreDictionary.registerOre("listAllfishraw", ItemParamblypterusRaw.block);
+		OreDictionary.registerOre("listAllmeatraw", ItemParamblypterusRaw.block);
+		OreDictionary.registerOre("foodMeat", ItemParamblypterusRaw.block);
+	}
+	public static class ItemFoodCustom extends ItemPNTaxidermyItem {
+		public ItemFoodCustom() {
+			super(2, 0.1f, false);
+			setTranslationKey("pf_paramblypterus_raw");
+			setRegistryName("paramblypterus_raw");
+			setCreativeTab(TabLepidodendronMobile.tab);
+			setMaxStackSize(64);
+		}
+	}
+}
--- src/main/java/net/lepidodendron/item/entities/ItemThanahitaRaw.java
+++ src/main/java/net/lepidodendron/item/entities/ItemThanahitaRaw.java
@@ -1,54 +1,54 @@
-//
-//package net.lepidodendron.item.entities;
-//
-//import net.lepidodendron.ElementsLepidodendronMod;
-//import net.lepidodendron.LepidodendronSorter;
-//import net.lepidodendron.creativetab.TabLepidodendronMobile;
-//import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-//import net.minecraft.item.Item;
-//import net.minecraftforge.client.event.ModelRegistryEvent;
-//import net.minecraftforge.client.model.ModelLoader;
-//import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-//import net.minecraftforge.fml.common.registry.GameRegistry;
-//import net.minecraftforge.fml.relauncher.Side;
-//import net.minecraftforge.fml.relauncher.SideOnly;
-//import net.minecraftforge.oredict.OreDictionary;
-//
-//@ElementsLepidodendronMod.ModElement.Tag
-//public class ItemThanahitaRaw extends ElementsLepidodendronMod.ModElement {
-//	@GameRegistry.ObjectHolder("lepidodendron:thanahita_raw")
-//	public static final Item block = null;
-//	public ItemThanahitaRaw(ElementsLepidodendronMod instance) {
-//		super(instance, LepidodendronSorter.thanahita_raw);
-//	}
-//
-//	@Override
-//	public void initElements() {
-//		elements.items.add(() -> new ItemFoodCustom());
-//	}
-//
-//	@SideOnly(Side.CLIENT)
-//	@Override
-//	public void registerModels(ModelRegistryEvent event) {
-//		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("lepidodendron:entities/thanahita_raw", "inventory"));
-//	}
-//
-//	@Override
-//	public void init(FMLInitializationEvent event) {
-//		super.init(event);
-//		OreDictionary.registerOre("mobdnaPNlepidodendron:prehistoric_flora_thanahita", ItemThanahitaRaw.block);
-//		OreDictionary.registerOre("listAllfishraw", ItemThanahitaRaw.block);
-//		OreDictionary.registerOre("listAllmeatraw", ItemThanahitaRaw.block);
-//		OreDictionary.registerOre("foodMeat", ItemThanahitaRaw.block);
-//	}
-//
-//	public static class ItemFoodCustom extends ItemPNTaxidermyItem {
-//		public ItemFoodCustom() {
-//			super(2, 0.1f, false);
-//			setTranslationKey("pf_thanahita_raw");
-//			setRegistryName("thanahita_raw");
-//			setCreativeTab(TabLepidodendronMobile.tab);
-//			setMaxStackSize(64);
-//		}
-//	}
-//}
+
+package net.lepidodendron.item.entities;
+
+import net.lepidodendron.ElementsLepidodendronMod;
+import net.lepidodendron.LepidodendronSorter;
+import net.lepidodendron.creativetab.TabLepidodendronMobile;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
+
+@ElementsLepidodendronMod.ModElement.Tag
+public class ItemThanahitaRaw extends ElementsLepidodendronMod.ModElement {
+	@GameRegistry.ObjectHolder("lepidodendron:thanahita_raw")
+	public static final Item block = null;
+	public ItemThanahitaRaw(ElementsLepidodendronMod instance) {
+		super(instance, LepidodendronSorter.thanahita_raw);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemFoodCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("lepidodendron:entities/thanahita_raw", "inventory"));
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
+		OreDictionary.registerOre("mobdnaPNlepidodendron:prehistoric_flora_thanahita", ItemThanahitaRaw.block);
+		OreDictionary.registerOre("listAllfishraw", ItemThanahitaRaw.block);
+		OreDictionary.registerOre("listAllmeatraw", ItemThanahitaRaw.block);
+		OreDictionary.registerOre("foodMeat", ItemThanahitaRaw.block);
+	}
+
+	public static class ItemFoodCustom extends ItemPNTaxidermyItem {
+		public ItemFoodCustom() {
+			super(2, 0.1f, false);
+			setTranslationKey("pf_thanahita_raw");
+			setRegistryName("thanahita_raw");
+			setCreativeTab(TabLepidodendronMobile.tab);
+			setMaxStackSize(64);
+		}
+	}
+}
