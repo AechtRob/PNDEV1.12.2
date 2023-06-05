2f0b35ea264a5bdd0b02e25bb312460bb48461e0
AechtRob <aecht_rob@outlook.com>
2023-06-05 12:19:46 +0100

    Tweak

--- src/main/java/net/lepidodendron/block/BlockPalaeognetaleana.java
+++ src/main/java/net/lepidodendron/block/BlockPalaeognetaleana.java
@@ -128,7 +128,7 @@ public class BlockPalaeognetaleana extends ElementsLepidodendronMod.ModElement {
 		@Override
 	    public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced) {
 	        if (LepidodendronConfig.showTooltips) {
-				tooltip.add("Type: Gnetale vine");
+				tooltip.add("Type: Gnetophyte vine");
 				tooltip.add("Periods: Permian [- Triassic (?) - Jurassic (?) - Cretaceous (?) - Paleogene (?) - Neogene (?) - Pleistocene (?) - present (?)]]");
 				tooltip.add("Propagation: fruit/cone");}
 	        super.addInformation(stack, player, tooltip, advanced);
