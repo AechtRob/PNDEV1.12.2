4b6b2099359e6624a559754cc0a2ef242c7ed495
AechtRob <aecht_rob@outlook.com>
2023-06-05 11:20:50 +0100

    Geohryostegus meat fix

--- src/main/java/net/lepidodendron/item/entities/ItemGephyrostegusRaw.java
+++ src/main/java/net/lepidodendron/item/entities/ItemGephyrostegusRaw.java
@@ -8,9 +8,11 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 
 @ElementsLepidodendronMod.ModElement.Tag
 public class ItemGephyrostegusRaw extends ElementsLepidodendronMod.ModElement {
@@ -31,6 +33,14 @@ public class ItemGephyrostegusRaw extends ElementsLepidodendronMod.ModElement {
 	public void registerModels(ModelRegistryEvent event) {
 		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("lepidodendron:entities/gephyrostegus_raw", "inventory"));
 	}
+
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
+		OreDictionary.registerOre("mobdnaPNlepidodendron:prehistoric_flora_gephryostegus", ItemGephyrostegusRaw.block);
+		OreDictionary.registerOre("listAllmeatraw", ItemGephyrostegusRaw.block);
+		OreDictionary.registerOre("foodMeat", ItemGephyrostegusRaw.block);
+	}
+
 	public static class ItemFoodCustom extends ItemPNTaxidermyItem {
 		public ItemFoodCustom() {
 			super(2, 0.1f, false);
