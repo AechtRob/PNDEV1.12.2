4e62d37cac136bd059794841da6f78df5574f94b
jyang077 <112518069+jyang077@users.noreply.github.com>
2023-07-31 12:14:43 -0400

    Entry and fixes

--- src/main/java/net/lepidodendron/LepidodendronBookSubscribers.java
+++ src/main/java/net/lepidodendron/LepidodendronBookSubscribers.java
@@ -11059,6 +11059,16 @@ public class LepidodendronBookSubscribers {
 				event.setCanceled(true);
 				return;
 			}
+
+			else if (event.getTarget() instanceof EntityPrehistoricFloraAngustidontus) {
+				if ((event.getEntityPlayer() instanceof EntityPlayerMP)) {
+					ModTriggers.CLICK_ANGUSTIDONTUS.trigger((EntityPlayerMP) event.getEntityPlayer());
+				}
+				event.getEntityPlayer().swingArm(event.getHand());
+				deliverStatsEntity(event);
+				event.setCanceled(true);
+				return;
+			}
 			else if (event.getTarget() instanceof EntityPrehistoricFloraJeholotriton) {
 				if ((event.getEntityPlayer() instanceof EntityPlayerMP)) {
 					ModTriggers.CLICK_JEHOLOTRITON.trigger((EntityPlayerMP) event.getEntityPlayer());
