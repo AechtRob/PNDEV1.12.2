821f3f6145216cbc32839d83aff22971f3a84fd8
AechtRob <aecht_rob@outlook.com>
2023-07-31 19:38:49 +0100

    v.58 Palaeopedia and spawns work

--- src/main/java/net/lepidodendron/util/SpawnLocations3.java
+++ src/main/java/net/lepidodendron/util/SpawnLocations3.java
@@ -1,15 +1,7 @@
 package net.lepidodendron.util;
 
-import net.lepidodendron.world.biome.EntityLists;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.biome.Biome;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import vazkii.patchouli.api.IComponentProcessor;
 import vazkii.patchouli.api.IVariableProvider;
-
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class SpawnLocations3 implements IComponentProcessor {
 
@@ -22,7 +14,7 @@ public class SpawnLocations3 implements IComponentProcessor {
 
     @Override
     public String process(String s) {
-        String biomeList = getBiomeList(this.mob);
+        String biomeList = SpawnLocations1.getBiomeList(this.mob, 32, 49);
         if (biomeList != null) {
             if (!biomeList.equalsIgnoreCase("")) {
                 return biomeList;
@@ -31,55 +23,4 @@ public class SpawnLocations3 implements IComponentProcessor {
         return "";
     }
 
-    @Nullable
-    public static String getBiomeList(String mobID) {
-        String biomeList = "; ";
-        String biomeListFinal = "; ";
-        int i = 0;
-        ArrayList<String> biomeArray = new ArrayList<String>();
-        for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
-            biomeArray.add(biome.getRegistryName().toString());
-        }
-        if (!biomeArray.isEmpty()) {
-            Collections.sort(biomeArray);
-            for (String biomeID : biomeArray) {
-                String biomeName = ForgeRegistries.BIOMES.getValue(new ResourceLocation(biomeID)).getBiomeName();
-                if (biomeName.length() > 0 && !biomeName.equalsIgnoreCase("null")) {
-                    if (!biomeList.contains("; " + biomeName + "; ")) {
-                        if (spawnsHere(mobID, biomeID)) {
-                            i ++;
-                            biomeList = biomeList + biomeName + "; ";
-                            if (i > 31 && i <= 47) {
-                                biomeListFinal = biomeListFinal + biomeName + "; ";
-                            }
-                        }
-                    }
-                }
-            }
-            if (!biomeListFinal.equalsIgnoreCase("; ")) {
-                biomeListFinal = biomeListFinal.substring(2);
-                if (i <= 47) {
-                    biomeListFinal = biomeListFinal.substring(0, biomeListFinal.length() - 2);
-                }
-                return biomeListFinal;
-            }
-        }
-        return "";
-    }
-
-    public static boolean spawnsHere(String mobID, String biomeID) {
-        Biome biome = ForgeRegistries.BIOMES.getValue(new ResourceLocation(biomeID));
-        String[] possibleMobs = EntityLists.mobString(biome, 0);
-        return matchMob(mobID + ":", possibleMobs) || matchMob(mobID + "{", possibleMobs);
-    }
-
-    public static boolean matchMob(String mob, String[] mobList) {
-
-        for (String mobFromList : mobList) {
-            if (mobFromList.contains(mob)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
