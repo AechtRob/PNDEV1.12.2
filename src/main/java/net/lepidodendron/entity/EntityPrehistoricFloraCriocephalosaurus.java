61bb4c348c9d3bf4554d9d0f763f8dcf683a1f83
AechtRob <aecht_rob@outlook.com>
2023-08-19 10:51:50 +0100

    v.58 Criocephalosaurus animations

--- src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraCriocephalosaurus.java
+++ src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraCriocephalosaurus.java
@@ -84,7 +84,7 @@ public class EntityPrehistoricFloraCriocephalosaurus extends EntityPrehistoricFl
 	}
 
 	public float getAISpeedLand() {
-		float speedBase = 0.3275F;
+		float speedBase = 0.3175F;
 		if (this.getTicks() < 0) {
 			return 0.0F; //Is laying eggs
 		}
@@ -92,7 +92,7 @@ public class EntityPrehistoricFloraCriocephalosaurus extends EntityPrehistoricFl
 			return 0.0F;
 		}
 		if (this.getIsFast()) {
-			speedBase = speedBase * 1.66F;
+			speedBase = speedBase * 1.86F;
 		}
 		return speedBase;
 	}
