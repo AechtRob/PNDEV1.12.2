cb09b447c7ff064c22ff6b9037871bfeed41bff2
AechtRob <aecht_rob@outlook.com>
2023-09-10 22:14:49 +0100

    v.58 Fixes

--- src/main/java/net/lepidodendron/item/entities/ItemPycnophlebiaRawF.java
+++ src/main/java/net/lepidodendron/item/entities/ItemPycnophlebiaRawF.java
@@ -4,6 +4,7 @@ package net.lepidodendron.item.entities;
 import net.lepidodendron.ElementsLepidodendronMod;
 import net.lepidodendron.LepidodendronSorter;
 import net.lepidodendron.creativetab.TabLepidodendronMobile;
+import net.lepidodendron.item.ItemGlassCaseDisplayItem;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -37,16 +38,22 @@ public class ItemPycnophlebiaRawF extends ElementsLepidodendronMod.ModElement {
 	public void init(FMLInitializationEvent event) {
 		super.init(event);
 		OreDictionary.registerOre("mobdnaPNlepidodendron:prehistoric_flora_pycnophlebia@female", ItemPycnophlebiaRawF.block);
-		OreDictionary.registerOre("pnfurnaceBug", ItemPycnophlebiaRaw.block);
-		OreDictionary.registerOre("pndietBug", ItemPycnophlebiaRaw.block);
+		OreDictionary.registerOre("pnfurnaceBug", ItemPycnophlebiaRawF.block);
+		OreDictionary.registerOre("pndietBug", ItemPycnophlebiaRawF.block);
 	}
-	public static class ItemFoodCustom extends ItemPNTaxidermyItem {
+	public static class ItemFoodCustom extends ItemGlassCaseDisplayItem {
 		public ItemFoodCustom() {
-			super(1, 0.05f, false);
+			super();
 			setTranslationKey("pf_pycnophlebia_raw_f");
 			setRegistryName("pycnophlebia_raw_f");
 			setCreativeTab(TabLepidodendronMobile.tab);
 			setMaxStackSize(64);
+		}
+
+		@Nullable
+		@Override
+		public String getMobStr() {
+			return "lepidodendron:prehistoric_flora_pycnophlebia";
 		}
 
 		@Nullable
