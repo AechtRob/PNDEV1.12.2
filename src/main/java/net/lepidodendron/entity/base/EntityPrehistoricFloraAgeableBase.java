4a5492eb10ee5ed07eedfd86b68f80a1c568f83e
AechtRob <aecht_rob@outlook.com>
2023-09-16 16:20:25 +0100

    v.58 Fixes

--- src/main/java/net/lepidodendron/entity/base/EntityPrehistoricFloraAgeableBase.java
+++ src/main/java/net/lepidodendron/entity/base/EntityPrehistoricFloraAgeableBase.java
@@ -691,9 +691,9 @@ public abstract class EntityPrehistoricFloraAgeableBase extends EntityTameable i
             nbttagcompound.setString("PNType", variantStr);
         }
 
-        if (s.substring(0, 10).equalsIgnoreCase("minecraft:")) {
+        //if (s.substring(0, 10).equalsIgnoreCase("minecraft:")) {
             nbttagcompound.setBoolean("PersistenceRequired", true);
-        }
+        //}
 
         nbttagcompound.setString("id", s);
         Entity entity = AnvilChunkLoader.readWorldEntityPos(nbttagcompound, worldIn, xpos, ypos, zpos, true);
