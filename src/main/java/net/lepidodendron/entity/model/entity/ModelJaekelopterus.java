68af7dfa0e2f57ce749f62715f77dad2db7dbcb1
AechtRob <aecht_rob@outlook.com>
2023-11-05 18:48:13 +0000

    v.59.0 Fixes

--- src/main/java/net/lepidodendron/entity/model/entity/ModelJaekelopterus.java
+++ src/main/java/net/lepidodendron/entity/model/entity/ModelJaekelopterus.java
@@ -401,7 +401,7 @@ public class ModelJaekelopterus extends AdvancedModelBase {
     public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity e) {
         super.setRotationAngles(f, f1, f2, f3, f4, f5, e);
         this.resetToDefaultPose();
-        this.body.offsetY = 0F;
+        this.body.offsetY = -0.28F;
 
         float speedMultiplier = 1F;
         EntityPrehistoricFloraAgeableBase ee = (EntityPrehistoricFloraAgeableBase) e;
