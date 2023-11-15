5ec0e8d1ad6d4b51bb1bc78dac9b37b46a7a9299
Tethyshadros <blackfrog@hotmail.it>
2023-11-15 20:10:41 +0100

    Microphymaspis and Thelodus scaler

--- src/main/java/net/lepidodendron/entity/render/entity/RenderThelodus.java
+++ src/main/java/net/lepidodendron/entity/render/entity/RenderThelodus.java
@@ -1,14 +1,19 @@
 package net.lepidodendron.entity.render.entity;
 
 import net.lepidodendron.LepidodendronMod;
+import net.lepidodendron.entity.EntityPrehistoricFloraGuiyu;
 import net.lepidodendron.entity.EntityPrehistoricFloraThelodus;
 import net.lepidodendron.entity.model.entity.ModelThelodus;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderThelodus extends RenderLiving<EntityPrehistoricFloraThelodus> {
     public static final ResourceLocation TEXTURE = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/thelodus.png");
+    public static float getScaler() {
+        return 0.5F;
+    }
 
     public RenderThelodus(RenderManager mgr) {
         super(mgr, new ModelThelodus(), 0.0f);
@@ -22,6 +27,15 @@ public class RenderThelodus extends RenderLiving<EntityPrehistoricFloraThelodus>
     @Override
     protected void applyRotations(EntityPrehistoricFloraThelodus entityLiving, float ageInTicks, float rotationYaw, float partialTicks) {
         super.applyRotations(entityLiving, ageInTicks, rotationYaw, partialTicks);
+    }
+    @Override
+    protected void preRenderCallback(EntityPrehistoricFloraThelodus entity, float f) {
+        float scale = this.getScaler();
+        if (scale < 0.1f) {
+            scale = 0.1f;
+        }
+        GlStateManager.scale(scale, scale, scale);
+        this.shadowSize = 0;
     }
 
 }
