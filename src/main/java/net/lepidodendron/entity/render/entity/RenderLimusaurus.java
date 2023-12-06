14996fa2ed43985e74f1549fde2b537c8aa53e4b
C0REZY <130365835+C0REZY@users.noreply.github.com>
2023-12-06 23:26:17 +0000

    Limusaurus Taxidermy Plateosaurus Taxidermy Herrerasaurus Taxidery Apatosaurus Taxidermy

--- src/main/java/net/lepidodendron/entity/render/entity/RenderLimusaurus.java
+++ src/main/java/net/lepidodendron/entity/render/entity/RenderLimusaurus.java
@@ -1,9 +1,12 @@
 package net.lepidodendron.entity.render.entity;
 
 import net.lepidodendron.LepidodendronMod;
+import net.lepidodendron.entity.EntityPrehistoricFloraCompsognathus;
 import net.lepidodendron.entity.EntityPrehistoricFloraLimusaurus;
 import net.lepidodendron.entity.model.entity.ModelLimusaurus;
+import net.lepidodendron.entity.render.tile.RenderDisplays;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
@@ -18,7 +21,36 @@ public class RenderLimusaurus extends RenderLiving<EntityPrehistoricFloraLimusau
     public static float getScaler() {
         return 0.3636F;
     }
-
+    @Override
+    public void doRender(EntityPrehistoricFloraLimusaurus entity, double x, double y, double z, float entityYaw, float partialTicks) {
+        @SuppressWarnings("deprecation")
+        Class clazz = sun.reflect.Reflection.getCallerClass(4);
+        if (clazz.getName().equalsIgnoreCase("vazkii.patchouli.client.book.page.PageEntity")) {
+            GlStateManager.pushMatrix();
+            GlStateManager.disableCull();
+            GlStateManager.enableAlpha();
+            boolean flag = this.setDoRenderBrightness(entity, partialTicks);
+            if (!this.bindEntityTexture(entity))
+            {
+                return;
+            }
+            RenderDisplays.modelLimusaurus.renderStaticBook(this.prepareScale(entity, partialTicks));
+            if (flag)
+            {
+                this.unsetBrightness();
+            }
+            GlStateManager.depthMask(true);
+            GlStateManager.disableRescaleNormal();
+            GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+            GlStateManager.enableTexture2D();
+            GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+            GlStateManager.enableCull();
+            GlStateManager.popMatrix();
+        }
+        else {
+            super.doRender(entity, x, y, z, entityYaw, partialTicks);
+        }
+    }
     @Override
     public ResourceLocation getEntityTexture(EntityPrehistoricFloraLimusaurus entity) {
         return RenderLimusaurus.TEXTURE;
