7b4115c1f807923e2dc73928f71700f990fd0fc8
AechtRob <aecht_rob@outlook.com>
2023-12-20 10:26:07 +0000

    v.59.0 Fixes

--- src/main/java/net/lepidodendron/entity/render/tile/RenderDisplayWallMount.java
+++ src/main/java/net/lepidodendron/entity/render/tile/RenderDisplayWallMount.java
@@ -541,6 +541,29 @@ public class RenderDisplayWallMount extends TileEntitySpecialRenderer<BlockDispl
         }
 
         if (facing == EnumFacing.DOWN) {
+            if (!transparent) {
+                //Arrow to show location:
+                if (ClientProxyLepidodendronMod.keyB.isKeyDown()) {
+                    GL11.glPushMatrix();
+                    GL11.glLineWidth(12);
+                    GL11.glDisable(GL11.GL_TEXTURE_2D);
+                    GL11.glColor3ub((byte) 255, (byte) 0, (byte) 0);
+                    GL11.glBegin(GL11.GL_LINES);
+                    GL11.glVertex3f((float) x + 0.5F, (float) y + 1 - 0.125F, (float) z + 0.5F);
+                    GL11.glVertex3f((float) x + 0.5F, (float) y + 1 - 1.5F, (float) z + 0.5F);
+                    GL11.glEnd();
+                    GL11.glBegin(GL11.GL_LINES);
+                    GL11.glVertex3f((float) x + 0.5F, (float) y + 1 - 0.125F, (float) z + 0.5F);
+                    GL11.glVertex3f((float) x + 0.5F + 0.5F, (float) y + 1 - 0.5F, (float) z + 0.5F);
+                    GL11.glEnd();
+                    GL11.glBegin(GL11.GL_LINES);
+                    GL11.glVertex3f((float) x + 0.5F, (float) y + 1 - 0.125F, (float) z + 0.5F);
+                    GL11.glVertex3f((float) x + 0.5F - 0.5F, (float) y + 1 - 0.5F, (float) z + 0.5F);
+                    GL11.glEnd();
+                    GL11.glEnable(GL11.GL_TEXTURE_2D);
+                    GL11.glPopMatrix();
+                }
+            }
             Method renderMethod = testAndGetMethod(model.getClass(), "renderStaticSuspended", new Class[]{float.class});
             if (renderMethod != null) {
                 if (!transparent) {
