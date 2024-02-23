a2f7b8d067e1246796d38ffdb89c81f72d5a0f61
Tethyshadros <blackfrog@hotmail.it>
2024-02-23 14:28:38 +0100

    Heliopeltis scaler

--- src/main/java/net/lepidodendron/entity/render/entity/RenderHeliopeltis.java
+++ src/main/java/net/lepidodendron/entity/render/entity/RenderHeliopeltis.java
@@ -1,14 +1,19 @@
 package net.lepidodendron.entity.render.entity;
 
 import net.lepidodendron.LepidodendronMod;
+import net.lepidodendron.entity.EntityPrehistoricFloraDicranurus;
 import net.lepidodendron.entity.EntityPrehistoricFloraHeliopeltis;
 import net.lepidodendron.entity.model.entity.ModelHeliopeltis;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderHeliopeltis extends RenderLiving<EntityPrehistoricFloraHeliopeltis> {
     public static final ResourceLocation TEXTURE = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/heliopeltis.png");
+    public static float getScaler() {
+        return 0.13F;
+    }
 
     public RenderHeliopeltis(RenderManager mgr) {
         super(mgr, new ModelHeliopeltis(), 0.0f);
@@ -22,6 +27,15 @@ public class RenderHeliopeltis extends RenderLiving<EntityPrehistoricFloraHeliop
     @Override
     protected void applyRotations(EntityPrehistoricFloraHeliopeltis entityLiving, float ageInTicks, float rotationYaw, float partialTicks) {
         super.applyRotations(entityLiving, ageInTicks, rotationYaw, partialTicks);
+    }
+    @Override
+    protected void preRenderCallback(EntityPrehistoricFloraHeliopeltis entity, float f) {
+        float scale = this.getScaler();
+        if (scale < 0.1f) {
+            scale = 0.1f;
+        }
+        GlStateManager.scale(scale, scale, scale);
+        this.shadowSize = 0;
     }
 
 }
