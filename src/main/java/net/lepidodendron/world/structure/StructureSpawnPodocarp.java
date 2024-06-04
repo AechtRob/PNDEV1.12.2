b57064443c72c33e0671aa4e87d91261afd21286
AechtRob <aecht_rob@outlook.com>
2024-06-04 22:09:22 +0100

    v.60 Bugfixes

--- src/main/java/net/lepidodendron/world/structure/StructureSpawnPodocarp.java
+++ src/main/java/net/lepidodendron/world/structure/StructureSpawnPodocarp.java
@@ -7,6 +7,8 @@ import net.lepidodendron.LepidodendronConfig;
 import net.lepidodendron.LepidodendronConfigPlants;
 import net.lepidodendron.LepidodendronDecorationHandler;
 import net.lepidodendron.procedure.ProcedureWorldGenPodocarp;
+import net.lepidodendron.procedure.ProcedureWorldGenRissikia;
+import net.lepidodendron.procedure.ProcedureWorldGenScarburgia;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -188,7 +190,15 @@ public class StructureSpawnPodocarp extends ElementsLepidodendronMod.ModElement 
 						world.setBlockToAir(spawnTo);
 						world.setBlockToAir(spawnTo.up());
 					}
-					ProcedureWorldGenPodocarp.executeProcedure($_dependencies);
+					if (random.nextBoolean()) {
+						ProcedureWorldGenPodocarp.executeProcedure($_dependencies);
+					}
+					else if (random.nextBoolean()) {
+						ProcedureWorldGenScarburgia.executeProcedure($_dependencies);
+					}
+					else {
+						ProcedureWorldGenRissikia.executeProcedure($_dependencies);
+					}
 				}
 
 			}
