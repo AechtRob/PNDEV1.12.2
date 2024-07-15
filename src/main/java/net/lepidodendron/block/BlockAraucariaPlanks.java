0e5ee050a6d68e79021b770d880e078da35f1dcd
AechtRob <aecht_rob@outlook.com>
2024-07-15 09:01:34 +0100

    v.60 fixes

--- src/main/java/net/lepidodendron/block/BlockAraucariaPlanks.java
+++ src/main/java/net/lepidodendron/block/BlockAraucariaPlanks.java
@@ -2,26 +2,19 @@
 package net.lepidodendron.block;
 
 import net.lepidodendron.ElementsLepidodendronMod;
-import net.lepidodendron.LepidodendronConfig;
 import net.lepidodendron.LepidodendronSorter;
 import net.lepidodendron.block.base.BlockPlanksPF;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
-
-import java.util.List;
 
 @ElementsLepidodendronMod.ModElement.Tag
 public class BlockAraucariaPlanks extends ElementsLepidodendronMod.ModElement {
@@ -49,24 +42,24 @@ public class BlockAraucariaPlanks extends ElementsLepidodendronMod.ModElement {
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
 				new ModelResourceLocation("lepidodendron:monkey_puzzle_planks", "inventory"));
 	}
+
 	public static class BlockCustom extends BlockPlanksPF {
 		public BlockCustom() {
 			setTranslationKey("pf_monkey_puzzle_planks");
 		}
 
-
-		@SideOnly(Side.CLIENT)
-		@Override
-		public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced) {
-			if (LepidodendronConfig.showTooltips) {
-				if (!Loader.isModLoaded("pncretaceousearly")) {
-					tooltip.add("NOTE: Used to build the portal to the Early Cretaceous dimension but you do not have that dimension mod installed");
-				} else {
-					tooltip.add("NOTE: Used to build the portal to the Early Cretaceous dimension");
-				}
-				super.addInformation(stack, player, tooltip, advanced);
-			}
-		}
+//		@SideOnly(Side.CLIENT)
+//		@Override
+//		public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced) {
+//			if (LepidodendronConfig.showTooltips) {
+//				if (!Loader.isModLoaded("pncretaceousearly")) {
+//					tooltip.add("NOTE: Used to build the portal to the Early Cretaceous dimension but you do not have that dimension mod installed");
+//				} else {
+//					tooltip.add("NOTE: Used to build the portal to the Early Cretaceous dimension");
+//				}
+//				super.addInformation(stack, player, tooltip, advanced);
+//			}
+//		}
 	}
 
 }
--- src/main/java/net/lepidodendron/block/BlockZirconGlassDoor.java
+++ src/main/java/net/lepidodendron/block/BlockZirconGlassDoor.java
@@ -8,6 +8,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.entity.player.EntityPlayer;
@@ -15,6 +16,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.translation.I18n;
@@ -106,6 +108,92 @@ public class BlockZirconGlassDoor extends ElementsLepidodendronMod.ModElement {
 	    {
 	        return state.getValue(HALF) == EnumDoorHalf.UPPER ? Items.AIR : (new ItemStack(ItemZirconGlassDoorItem.block, (int) (1))).getItem();
 	    }
+
+		@Override
+		public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos)
+		{
+			if (state.getValue(HALF) == BlockDoor.EnumDoorHalf.UPPER)
+			{
+				BlockPos blockpos = pos.down();
+				IBlockState iblockstate = worldIn.getBlockState(blockpos);
+
+				if (iblockstate.getBlock() != this)
+				{
+					worldIn.setBlockToAir(pos);
+				}
+				else if (blockIn != this)
+				{
+					iblockstate.neighborChanged(worldIn, blockpos, blockIn, fromPos);
+				}
+			}
+			else
+			{
+				boolean flag1 = false;
+				BlockPos blockpos1 = pos.up();
+				IBlockState iblockstate1 = worldIn.getBlockState(blockpos1);
+
+				if (iblockstate1.getBlock() != this)
+				{
+					worldIn.setBlockToAir(pos);
+					flag1 = true;
+				}
+
+				if (!isSideSolidForDoor(worldIn, pos.down(), EnumFacing.UP))
+				{
+					worldIn.setBlockToAir(pos);
+					flag1 = true;
+
+					if (iblockstate1.getBlock() == this)
+					{
+						worldIn.setBlockToAir(blockpos1);
+					}
+				}
+
+				if (flag1)
+				{
+					if (!worldIn.isRemote)
+					{
+						this.dropBlockAsItem(worldIn, pos, state, 0);
+					}
+				}
+				else
+				{
+					boolean flag = worldIn.isBlockPowered(pos) || worldIn.isBlockPowered(blockpos1);
+
+					if (blockIn != this && (flag || blockIn.getDefaultState().canProvidePower()) && flag != ((Boolean)iblockstate1.getValue(POWERED)).booleanValue())
+					{
+						worldIn.setBlockState(blockpos1, iblockstate1.withProperty(POWERED, Boolean.valueOf(flag)), 2);
+
+						if (flag != ((Boolean)state.getValue(OPEN)).booleanValue())
+						{
+							worldIn.setBlockState(pos, state.withProperty(OPEN, Boolean.valueOf(flag)), 2);
+							worldIn.markBlockRangeForRenderUpdate(pos, pos);
+							worldIn.playEvent((EntityPlayer)null, flag ? 1006 : 1012, pos, 0);
+						}
+					}
+				}
+			}
+		}
+
+		@Override
+		public boolean canPlaceBlockAt(World worldIn, BlockPos pos)
+		{
+			if (pos.getY() >= worldIn.getHeight() - 1)
+			{
+				return false;
+			}
+			else
+			{
+				IBlockState state = worldIn.getBlockState(pos.down());
+				return (state.isTopSolid() || state.getBlockFaceShape(worldIn, pos.down(), EnumFacing.UP) == BlockFaceShape.SOLID) && worldIn.getBlockState(pos).getBlock().isReplaceable(worldIn, pos) && worldIn.getBlockState(pos.up()).getBlock().isReplaceable(worldIn, pos.up());
+			}
+		}
+
+		public static boolean isSideSolidForDoor(World world, BlockPos pos, EnumFacing facing) {
+
+			return world.getBlockState(pos).isSideSolid(world,  pos, facing)
+					|| (world.getBlockState(pos).getMaterial() == Material.GLASS && world.getBlockState(pos).getBlockFaceShape(world, pos, facing) == BlockFaceShape.SOLID);
+		}
 
 	}
 }
