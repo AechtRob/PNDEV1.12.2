30d4312f9f5cc6239b5af3a72d28a4059a972efd
AechtRob <aecht_rob@outlook.com>
2024-07-27 14:10:02 +0100

    v.60 fixes

--- src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraDeuterosaurus.java
+++ src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraDeuterosaurus.java
@@ -10,12 +10,9 @@ import net.lepidodendron.block.base.IAdvancementGranter;
 import net.lepidodendron.entity.ai.*;
 import net.lepidodendron.entity.base.EntityPrehistoricFloraAgeableBase;
 import net.lepidodendron.entity.base.EntityPrehistoricFloraLandBase;
-import net.lepidodendron.entity.render.entity.RenderAnteosaurus;
-import net.lepidodendron.entity.render.tile.RenderDisplays;
 import net.lepidodendron.entity.util.ITrappableLand;
 import net.lepidodendron.util.CustomTrigger;
 import net.lepidodendron.util.ModTriggers;
-import net.minecraft.client.model.ModelBase;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureAttribute;
@@ -347,56 +344,56 @@ public class EntityPrehistoricFloraDeuterosaurus extends EntityPrehistoricFloraL
 		}
 		return LepidodendronMod.DEUTEROSAURUS_LOOT;
 	}
-	public static double offsetWall(@Nullable String variant) {
-		return 0.01;
-	}
-	public static double upperfrontverticallinedepth(@Nullable String variant) {
-		return 1.4;
-	}
-	public static double upperbackverticallinedepth(@Nullable String variant) {
-		return 0.8;
-	}
-	public static double upperfrontlineoffset(@Nullable String variant) {
-		return 0.4;
-	}
-	public static double upperfrontlineoffsetperpendiular(@Nullable String variant) {
-		return -0F;
-	}
-	public static double upperbacklineoffset(@Nullable String variant) {
-		return 0.4;
-	}
-	public static double upperbacklineoffsetperpendiular(@Nullable String variant) {
-		return -0.15F;
-	}
-	public static double lowerfrontverticallinedepth(@Nullable String variant) {
-		return 0.9;
-	}
-	public static double lowerbackverticallinedepth(@Nullable String variant) {
-		return 0.9;
-	}
-	public static double lowerfrontlineoffset(@Nullable String variant) {
-		return 0.0;
-	}
-	public static double lowerfrontlineoffsetperpendiular(@Nullable String variant) {
-		return 0.45F;
-	}
-	public static double lowerbacklineoffset(@Nullable String variant) {
-		return 0.0;
-	}
-	public static double lowerbacklineoffsetperpendiular(@Nullable String variant) {
-		return -0.49F;
-	}
-	@SideOnly(Side.CLIENT)
-	public static ResourceLocation textureDisplay(@Nullable String variant) {
-		return RenderAnteosaurus.TEXTURE;
-	}
-	@SideOnly(Side.CLIENT)
-	public static ModelBase modelDisplay(@Nullable String variant) {
-		return RenderDisplays.modelAnteosaurus;
-	}
-	public static float getScaler(@Nullable String variant) {
-		return RenderAnteosaurus.getScaler();
-	}
+//	public static double offsetWall(@Nullable String variant) {
+//		return 0.01;
+//	}
+//	public static double upperfrontverticallinedepth(@Nullable String variant) {
+//		return 1.4;
+//	}
+//	public static double upperbackverticallinedepth(@Nullable String variant) {
+//		return 0.8;
+//	}
+//	public static double upperfrontlineoffset(@Nullable String variant) {
+//		return 0.4;
+//	}
+//	public static double upperfrontlineoffsetperpendiular(@Nullable String variant) {
+//		return -0F;
+//	}
+//	public static double upperbacklineoffset(@Nullable String variant) {
+//		return 0.4;
+//	}
+//	public static double upperbacklineoffsetperpendiular(@Nullable String variant) {
+//		return -0.15F;
+//	}
+//	public static double lowerfrontverticallinedepth(@Nullable String variant) {
+//		return 0.9;
+//	}
+//	public static double lowerbackverticallinedepth(@Nullable String variant) {
+//		return 0.9;
+//	}
+//	public static double lowerfrontlineoffset(@Nullable String variant) {
+//		return 0.0;
+//	}
+//	public static double lowerfrontlineoffsetperpendiular(@Nullable String variant) {
+//		return 0.45F;
+//	}
+//	public static double lowerbacklineoffset(@Nullable String variant) {
+//		return 0.0;
+//	}
+//	public static double lowerbacklineoffsetperpendiular(@Nullable String variant) {
+//		return -0.49F;
+//	}
+//	@SideOnly(Side.CLIENT)
+//	public static ResourceLocation textureDisplay(@Nullable String variant) {
+//		return RenderAnteosaurus.TEXTURE;
+//	}
+//	@SideOnly(Side.CLIENT)
+//	public static ModelBase modelDisplay(@Nullable String variant) {
+//		return RenderDisplays.modelAnteosaurus;
+//	}
+//	public static float getScaler(@Nullable String variant) {
+//		return RenderAnteosaurus.getScaler();
+//	}
 
 	@Nullable
 	@Override
