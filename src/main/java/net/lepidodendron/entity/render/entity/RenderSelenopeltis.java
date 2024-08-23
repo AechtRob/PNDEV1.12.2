a15be171b5737f762774b869e236e7b00675ac87
razor <razorrotom@gmail.com>
2024-08-23 13:08:07 -0400

    Selenopeltis remodel

--- src/main/java/net/lepidodendron/entity/render/entity/RenderSelenopeltis.java
+++ src/main/java/net/lepidodendron/entity/render/entity/RenderSelenopeltis.java
@@ -1,14 +1,20 @@
 package net.lepidodendron.entity.render.entity;
 
 import net.lepidodendron.LepidodendronMod;
+import net.lepidodendron.entity.EntityPrehistoricFloraSchoenesmahl;
 import net.lepidodendron.entity.EntityPrehistoricFloraSelenopeltis;
 import net.lepidodendron.entity.model.entity.ModelSelenopeltis;
 import net.lepidodendron.entity.render.RenderLivingBaseWithBook;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderSelenopeltis extends RenderLivingBaseWithBook<EntityPrehistoricFloraSelenopeltis> {
-    public static final ResourceLocation TEXTURE = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/Selenopeltis.png");
+    public static final ResourceLocation TEXTURE = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/selenopeltis.png");
+
+    public static float getScaler() {
+        return 0.25F;
+    }
 
     public RenderSelenopeltis(RenderManager mgr) {
         super(mgr, new ModelSelenopeltis(), 0.08f);
@@ -22,6 +28,16 @@ public class RenderSelenopeltis extends RenderLivingBaseWithBook<EntityPrehistor
     @Override
     protected void applyRotations(EntityPrehistoricFloraSelenopeltis entityLiving, float ageInTicks, float rotationYaw, float partialTicks) {
         super.applyRotations(entityLiving, ageInTicks, rotationYaw, partialTicks);
+    }
+
+    @Override
+    protected void preRenderCallback(EntityPrehistoricFloraSelenopeltis entity, float f) {
+        float scale = this.getScaler();
+        if (scale < 0.1f) {
+            scale = 0.1f;
+        }
+        GlStateManager.scale(scale, scale, scale);
+        this.shadowSize = 0;
     }
 
 }
