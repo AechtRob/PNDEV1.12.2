4a88791382f9f9c59c3c8dd603a5448fbdd0cdc7
AechtRob <aecht_rob@outlook.com>
2024-08-29 20:04:55 +0100

    v.61 Worldgeneration

--- src/main/java/net/lepidodendron/world/gen/WorldGenPrehistoricLakes.java
+++ src/main/java/net/lepidodendron/world/gen/WorldGenPrehistoricLakes.java
@@ -1,9 +1,6 @@
 package net.lepidodendron.world.gen;
 
-import net.lepidodendron.block.BlockBrownstone;
-import net.lepidodendron.block.BlockDriedMud;
-import net.lepidodendron.block.BlockPrehistoricGroundSand;
-import net.lepidodendron.block.BlockPrehistoricGroundSandBlack;
+import net.lepidodendron.block.*;
 import net.lepidodendron.entity.render.tile.RenderDisplayWallMount;
 import net.lepidodendron.util.Functions;
 import net.lepidodendron.world.biome.ChunkGenSpawner;
@@ -171,6 +168,9 @@ public class WorldGenPrehistoricLakes extends WorldGenerator
                                     || worldIn.getBiome(blockpos).getRegistryName().toString().equalsIgnoreCase("lepidodendron:jurassic_southern_taiga_hills")
                                     || worldIn.getBiome(blockpos).getRegistryName().toString().equalsIgnoreCase("lepidodendron:jurassic_southern_taiga_basalt")) {
                                     Functions.setBlockStateAndCheckForDoublePlant(worldIn,blockpos, BlockPrehistoricGroundSandBlack.block.getDefaultState(), 2);
+                                }
+                                if (worldIn.getBiome(blockpos).getRegistryName().toString().equalsIgnoreCase("lepidodendron:cretaceous_early_africa_valley")) {
+                                    Functions.setBlockStateAndCheckForDoublePlant(worldIn,blockpos, BlockPrehistoricGroundSandRed.block.getDefaultState(), 2);
                                 }
                             }
                             if (worldIn.getBlockState(blockpos).getBlock() == BlockBrownstone.block && worldIn.getLightFor(EnumSkyBlock.SKY, position.add(i2, j4, j3)) > 0)
