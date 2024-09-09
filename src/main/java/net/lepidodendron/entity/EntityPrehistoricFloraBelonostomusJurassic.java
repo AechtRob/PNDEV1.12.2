21ee237b18bc46a212914fb32537f858224a640c
razor <razorrotom@gmail.com>
2024-09-09 00:33:43 -0400

    Belonostomus update so that it is useable in both Jurassic and Cretaceous

--- src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraBelonostomusJurassic.java
+++ src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraBelonostomusJurassic.java
@@ -63,7 +63,7 @@ public class EntityPrehistoricFloraBelonostomusJurassic extends EntityPrehistori
 		return true;
 	}
 
-	public static String getPeriod() {return "Jurassic";}
+	public static String getPeriod() {return "Jurassic - Cretaceous";}
 
 	//public static String getHabitat() {return "Aquatic";}
 
