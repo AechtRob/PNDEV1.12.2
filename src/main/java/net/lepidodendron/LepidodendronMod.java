a903f7796b67ba2cd1de74d14545dce08e705762
AechtRob <aecht_rob@outlook.com>
2024-10-20 19:08:41 +0100

    v.61.02 lang fixes

--- src/main/java/net/lepidodendron/LepidodendronMod.java
+++ src/main/java/net/lepidodendron/LepidodendronMod.java
@@ -62,7 +62,7 @@ import java.util.function.Supplier;
 public class LepidodendronMod {
 	public static final String MODID = "lepidodendron";
 	public static final String NAME = "Prehistoric Nature";
-	public static final String VERSION = "61.02";
+	public static final String VERSION = "61.03";
 	public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("lepidodendron:a");
     @SidedProxy(clientSide = "net.lepidodendron.ClientProxyLepidodendronMod", serverSide = "net.lepidodendron.ServerProxyLepidodendronMod")
 	public static IProxyLepidodendronMod proxy;
