6264086d47ad52a983e8807b098dfd765a764eb7
C0REZY <130365835+C0REZY@users.noreply.github.com>
2024-11-24 18:26:41 +0000

    Cervifurca taxidermy tweak Dicranurus taxidermy tweak Dysalotosaurus taxidermy tweak

--- src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraDicranurus.java
+++ src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraDicranurus.java
@@ -155,39 +155,23 @@ public class EntityPrehistoricFloraDicranurus extends EntityPrehistoricFloraTril
 	protected ResourceLocation getLootTable() {
 		return LepidodendronMod.DICRANURUS_LOOT;
 	}
-
 	public static double offsetWall(@Nullable String variant) {return -1.36;}
-
 	public static double upperfrontverticallinedepth(@Nullable String variant) {return 2.0;}
-
 	public static double upperbackverticallinedepth(@Nullable String variant) {return 2.0;}
-
 	public static double upperfrontlineoffset(@Nullable String variant) {return 0.0;}
-
 	public static double upperfrontlineoffsetperpendiular(@Nullable String variant) {return -0.9F;}
-
 	public static double upperbacklineoffset(@Nullable String variant) {return 0.0;}
-
 	public static double upperbacklineoffsetperpendiular(@Nullable String variant) {return 1.4F;}
-
-	public static double lowerfrontverticallinedepth(@Nullable String variant) {return 2.9;}
-
-	public static double lowerbackverticallinedepth(@Nullable String variant) {return 2.0;}
-
+	public static double lowerfrontverticallinedepth(@Nullable String variant) {return 0.0;}
+	public static double lowerbackverticallinedepth(@Nullable String variant) {return 0.0;}
 	public static double lowerfrontlineoffset(@Nullable String variant) {return 0.0;}
-
 	public static double lowerfrontlineoffsetperpendiular(@Nullable String variant) {return 2.0F;}
-
 	public static double lowerbacklineoffset(@Nullable String variant) {return 0.0;}
-
 	public static double lowerbacklineoffsetperpendiular(@Nullable String variant) {return -0.5F;}
-
 	@SideOnly(Side.CLIENT)
 	public static ResourceLocation textureDisplay(@Nullable String variant) {return RenderDicranurus.TEXTURE;}
-
 	@SideOnly(Side.CLIENT)
 	public static ModelBase modelDisplay(@Nullable String variant) {return RenderDisplays.modelDicranurus;}
-
 	public static float getScaler(@Nullable String variant) {return RenderDicranurus.getScaler();}
 
 }
