ddbe71fdab1e1f8cd442fbe7b1d57d839968a91e
FunnyJamie <130430045+FunnyJamie@users.noreply.github.com>
2024-12-27 17:33:57 +1100

    size edit on drepano, machimo, tyranno

--- src/main/java/net/lepidodendron/entity/render/entity/RenderDrepanosaurus.java
+++ src/main/java/net/lepidodendron/entity/render/entity/RenderDrepanosaurus.java
@@ -12,7 +12,7 @@ public class RenderDrepanosaurus extends RenderLivingBaseWithBook<EntityPrehisto
     public static final ResourceLocation TEXTURE = new ResourceLocation(LepidodendronMod.MODID + ":textures/entities/drepanosaurus.png");
 
     public static float getScaler() {
-        return 0.12f;
+        return 0.15f;
     }
 
     public RenderDrepanosaurus(RenderManager mgr) {
