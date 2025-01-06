565af7424b2899ee03b2b4cba3f5761370044b84
AechtRob <aecht_rob@outlook.com>
2025-01-06 11:36:09 +0000

    v.62.0 Fixes

--- src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraHypophylloceras.java
+++ src/main/java/net/lepidodendron/entity/EntityPrehistoricFloraHypophylloceras.java
@@ -2,7 +2,6 @@
 package net.lepidodendron.entity;
 
 import net.ilexiconn.llibrary.client.model.tools.ChainBuffer;
-import net.lepidodendron.LepidodendronConfig;
 import net.lepidodendron.LepidodendronMod;
 import net.lepidodendron.block.base.IAdvancementGranter;
 import net.lepidodendron.entity.ai.DietString;
@@ -17,13 +16,10 @@ import net.lepidodendron.util.CustomTrigger;
 import net.lepidodendron.util.ModTriggers;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.commons.lang3.ArrayUtils;
@@ -122,32 +118,6 @@ public class EntityPrehistoricFloraHypophylloceras extends EntityPrehistoricFlor
 	@Override
 	public ItemStack getPropagule() {
 		return new ItemStack(ItemNautiloidEggsHypophylloceras.block, (int) (1));
-	}
-
-	@Override
-	public void onLivingUpdate() {
-		super.onLivingUpdate();
-		//this.renderYawOffset = this.rotationYaw;
-		if (this.isEntityAlive() && isInWater()) {
-			if (this.isAtBottom()) {
-				//Feeding from bottom pose:
-				BlockPos posIn = this.getPosition();
-				World worldIn = this.world;
-				//worldIn.playSound(player, posIn, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.5F, 2.6F + (worldIn.rand.nextFloat() - worldIn.rand.nextFloat()) * 0.8F);
-				if (worldIn instanceof WorldServer) {
-					if (Math.random() > 0.3) {
-						for (int k = 0; k < 8; ++k) {
-							((WorldServer) worldIn).spawnParticle(EnumParticleTypes.WATER_BUBBLE, posIn.getX(), posIn.getY(), posIn.getZ(), (int) 1, 1, 1, 1, 0.2, new int[0]);
-						}
-					}
-					if (Math.random() > 0.4) {
-						for (int k = 0; k < 4; ++k) {
-							((WorldServer) worldIn).spawnParticle(EnumParticleTypes.SMOKE_NORMAL, posIn.getX(), posIn.getY(), posIn.getZ(), (int) 1, 1, 1, 1, 0.1, new int[0]);
-						}
-					}
-				}
-			}
-		}
 	}
 
 	@Nullable
