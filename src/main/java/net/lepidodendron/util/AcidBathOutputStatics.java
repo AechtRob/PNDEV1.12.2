f2d7432d5842cf8c257ee13404794e1f4d6d9201
AechtRob <aecht_rob@outlook.com>
2025-12-04 16:52:34 +0000

    v.63 Remove Roveocrinus for now

--- src/main/java/net/lepidodendron/util/AcidBathOutputStatics.java
+++ src/main/java/net/lepidodendron/util/AcidBathOutputStatics.java
@@ -774,7 +774,7 @@ public class AcidBathOutputStatics {
                 ForgeRegistries.BLOCKS.getKey(BlockSeaPenYellow.block).toString(),
                 ForgeRegistries.BLOCKS.getKey(BlockBoneWorm.block).toString(),
                 ForgeRegistries.BLOCKS.getKey(BlockCrinoidHemicrinus.block).toString(),
-                ForgeRegistries.BLOCKS.getKey(BlockCrinoidRoveacrinus.block).toString(),
+                //ForgeRegistries.BLOCKS.getKey(BlockCrinoidRoveacrinus.block).toString(),
                 ForgeRegistries.BLOCKS.getKey(BlockBivalveRequienia.block).toString(),
                 ForgeRegistries.BLOCKS.getKey(BlockBivalveToucasia.block).toString(),
                 ForgeRegistries.BLOCKS.getKey(BlockCoeloptychium.block).toString()
