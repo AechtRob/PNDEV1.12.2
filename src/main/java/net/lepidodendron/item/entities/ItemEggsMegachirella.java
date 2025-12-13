f11c822e1152e90d939ad6e0f5f68357a61962a7
AechtRob <aecht_rob@outlook.com>
2025-12-13 09:15:09 +0000

    v.63.02 Bugfix release

---
+++ src/main/java/net/lepidodendron/item/entities/ItemEggsMegachirella.java
@@ -0,0 +1,64 @@
+
+package net.lepidodendron.item.entities;
+
+import net.lepidodendron.ElementsLepidodendronMod;
+import net.lepidodendron.LepidodendronMod;
+import net.lepidodendron.LepidodendronSorter;
+import net.lepidodendron.creativetab.TabLepidodendronMobile;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
+
+@ElementsLepidodendronMod.ModElement.Tag
+public class ItemEggsMegachirella extends ElementsLepidodendronMod.ModElement {
+	@GameRegistry.ObjectHolder("lepidodendron:eggs_megachirella")
+	public static final Item block = null;
+	public ItemEggsMegachirella(ElementsLepidodendronMod instance) {
+		super(instance, LepidodendronSorter.eggs_megachirella);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("lepidodendron:entities/eggs_megachirella", "inventory"));
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
+		OreDictionary.registerOre("bakingEgg", ItemEggsMegachirella.block);
+		OreDictionary.registerOre("egg", ItemEggsMegachirella.block);
+		OreDictionary.registerOre("eggCookable", ItemEggsMegachirella.block);
+		OreDictionary.registerOre("foodSimpleEgg", ItemEggsMegachirella.block);
+		OreDictionary.registerOre("ingredientEgg", ItemEggsMegachirella.block);
+		OreDictionary.registerOre("listAllegg", ItemEggsMegachirella.block);
+		OreDictionary.registerOre("objectEgg", ItemEggsMegachirella.block);
+	}
+
+	public static class ItemCustom extends ItemEggsPF {
+		public ItemCustom() {
+			super();
+			setTranslationKey("pf_eggs_megachirella");
+			setRegistryName("eggs_megachirella");
+			setCreativeTab(TabLepidodendronMobile.tab);
+		}
+
+		@Override
+		public String getMobString() {
+			return LepidodendronMod.MODID + ":prehistoric_flora_megachirella";
+		}
+
+	}
+
+}
