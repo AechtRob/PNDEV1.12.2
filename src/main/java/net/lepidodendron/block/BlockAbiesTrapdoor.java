56bc20b8616331e928c0a3c5d2232c7b7eb981d0
AechtRob <aecht_rob@outlook.com>
2026-01-22 09:07:02 +0000

    v.64.0 content

--- src/main/java/net/lepidodendron/block/BlockAbiesTrapdoor.java
+++ src/main/java/net/lepidodendron/block/BlockAbiesTrapdoor.java
@@ -17,9 +17,11 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 
 @ElementsLepidodendronMod.ModElement.Tag
 public class BlockAbiesTrapdoor extends ElementsLepidodendronMod.ModElement {
@@ -35,12 +37,19 @@ public class BlockAbiesTrapdoor extends ElementsLepidodendronMod.ModElement {
 		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
 	}
 
+	@Override
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
+		OreDictionary.registerOre("trapdoorWood", BlockAbiesTrapdoor.block);
+	}
+
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
 				new ModelResourceLocation("lepidodendron:abies_trapdoor", "inventory"));
 	}
+
 	public static class BlockCustom extends BlockTrapDoor {
 		public BlockCustom() {
 			super(Material.WOOD);
