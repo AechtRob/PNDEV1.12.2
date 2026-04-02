e2562d030a2cc319b79b495aac81f70649fe2ea9
AechtRob <aecht_rob@outlook.com>
2026-04-02 08:15:42 +0100

    v.64.0 content

--- src/main/java/net/lepidodendron/entity/model/entity/ModelKimberella.java
+++ src/main/java/net/lepidodendron/entity/model/entity/ModelKimberella.java
@@ -102,7 +102,7 @@ public class ModelKimberella extends ModelBasePalaeopedia {
     @Override
     public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity e) {
         super.setRotationAngles(f, f1, f2, f3, f4, f5, e);
-        this.resetToDefaultPose();
+        //this.resetToDefaultPose();
 
         EntityPrehistoricFloraKimberella ee = (EntityPrehistoricFloraKimberella) e;
 
