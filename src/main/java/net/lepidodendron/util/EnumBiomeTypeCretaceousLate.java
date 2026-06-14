20cc3031b0281d4f0fea2e52abbe754dad0a9c77
AechtRob <aecht_rob@outlook.com>
2026-06-14 20:00:25 +0100

    v.64.0 content

--- src/main/java/net/lepidodendron/util/EnumBiomeTypeCretaceousLate.java
+++ src/main/java/net/lepidodendron/util/EnumBiomeTypeCretaceousLate.java
@@ -8,11 +8,12 @@ public enum EnumBiomeTypeCretaceousLate implements IStringSerializable {
     Late_Cretaceous_Africa("late_cretaceous_africa"),
     Late_Cretaceous_Asia("late_cretaceous_asia"),
     Late_Cretaceous_Europe("late_cretaceous_europe"),
-    Late_Cretaceous_North_America("late_cretaceous_north_america"),
+    Late_Cretaceous_North_America_Laramidia("late_cretaceous_north_america_laramidia"),
+    Late_Cretaceous_North_America_Appalachia("late_cretaceous_north_america_appalachia"),
     Late_Cretaceous_South_America("late_cretaceous_south_america"),
+    Late_Cretaceous_Austro_Antarctica("late_cretaceous_austro_antarctica"),
 
     //Islands (i.e. in Deep Ocean):
-    Late_Cretaceous_Austro_Antarctica("late_cretaceous_austro_antarctica"),
     Late_Cretaceous_India("late_cretaceous_india"),
     Late_Cretaceous_Madagascar("late_cretaceous_madagascar"),
 
@@ -22,7 +23,8 @@ public enum EnumBiomeTypeCretaceousLate implements IStringSerializable {
     Late_Cretaceous_Ocean_Shore_Tethys_South("late_cretaceous_ocean_tethys_south"),
     Late_Cretaceous_Ocean_Shore_Pacific("late_cretaceous_ocean_pacific"),
     Late_Cretaceous_Ocean_Shore_Atlantic("late_cretaceous_ocean_atlantic"),
-    Late_Cretaceous_Ocean_Shore_Southern("late_cretaceous_ocean_southern");
+    Late_Cretaceous_Ocean_Shore_Southern("late_cretaceous_ocean_southern"),
+    Late_Cretaceous_Ocean_WIS("late_cretaceous_ocean_wis");
 
     private final String name;
 
